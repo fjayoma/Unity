571b46894480a1eed6a9c2fa96167fa5bc1aca04
fjayoma <francis.jayoma@gmail.com>
2022-04-23 20:49:11 -0500

    Collision Detection v2 4-23
    Improved Collision Detection. Removes bouncing off walls.

---
+++ myFirstGame/myFirstGameProgram/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+
+{
+    public float moveSpeed;
+    public Rigidbody2D myRigidbody; //prevents player from bouncing against collision walls
+
+    private Animator anim; 
+
+    private bool playerMoving;
+    private Vector2 lastMove;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); // first time the scene starts
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        playerMoving = false;
+
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) /* f equals float */
+            {
+                myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*moveSpeed,myRigidbody.velocity.y);
+                //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime,0f,0f)); //move player around the world 
+                
+                playerMoving = true;
+                lastMove = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
+            }
+        
+         if(Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) /* f equals float */
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x,Input.GetAxisRaw("Vertical")*moveSpeed);
+                //transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime,0f)); /*x,y,z value*/
+                
+                playerMoving = true;
+                lastMove = new Vector2(0f,Input.GetAxisRaw("Vertical"));
+            }
+
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f) //prevenets bouncing against walls
+            {
+                myRigidbody.velocity = new Vector2(0f,myRigidbody.velocity.y);
+            }
+        if(Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x,0f);
+            }
+
+
+            anim.SetFloat("MoveX",Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("MoveY",Input.GetAxisRaw("Vertical"));
+            anim.SetBool("PlayerMoving",playerMoving);
+            anim.SetFloat("LastMoveX",lastMove.x);
+            anim.SetFloat("LastMoveY",lastMove.y);
+    }
+}
--- test2/thisisatest2/Assets/Art/base_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="base_tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
- <image source="Desktop/Unity/Project1/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
-</tileset>
